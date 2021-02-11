c478711901b2247b9b8ac941ae69989b050a9825
pinkynail228 <erratum@ya.ru>
2021-02-11 22:44:33 +0300

    ver.1

--- src/componenta/Head1.jsx
+++ src/componenta/Head1.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import './../App.css'
+import React from "react"
+import cast from './Head1.Module.css';
 
 const Head1 = () => {
     return (
-        <header className='head'>
+        <header className={cast.head}>
+            {/* eslint-disable-next-line jsx-a11y/alt-text */}
             <img src='https://image.flaticon.com/icons/png/512/87/87400.png'/>
         </header>
     )
--- src/componenta/Profile.jsx
+++ src/componenta/Profile.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from "react"
 import './../App.css'
+import cast from './Profile.Module.css'
 
 const Profile = () => {
     return (
-        <div className='profile'>
+        <div className={cast.profile}>
+            {/* eslint-disable-next-line jsx-a11y/alt-text */}
             <div><img src='https://34travel.me/media/posts/5e1a25f3879e9-samara-pan.jpg'/>
             </div>
             <div>
